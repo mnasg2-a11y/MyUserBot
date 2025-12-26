4e42b6eb571428a06138febe1e7bfb1a1eb5969a
mnasg2-a11y <mnasg2@gmail.com>
2025-12-26 19:40:01 +0300

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -1,1 +1,9 @@
+from telethon import events
 
+@client.on(events.NewMessage(outgoing=True, pattern=r'\.test1'))
+async def test1_handler(event):
+    await event.edit("✅ هذا أمر جديد من test.py")
+
+@client.on(events.NewMessage(outgoing=True, pattern=r'\.test2'))
+async def test2_handler(event):
+    await event.edit("✅ أمر آخر من نفس الملف")
